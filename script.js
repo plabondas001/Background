651be75e6c87c3a9bceb1d0083b11f4434087c4d
plabondas001 <plabondas001@gmail.com>
2025-12-07 21:43:48 +0600

    change backgroundcolor

---
+++ script.js
@@ -0,0 +1,16 @@
+// Dark mode
+function darkMode() {
+    document.body.style.backgroundColor = 'black'
+    const first = document.getElementById('color')
+    first.style.color = 'white'
+    
+}
+
+
+
+// Light mode
+function lightMode() {
+    document.body.style.backgroundColor = 'white'
+    const second = document.getElementById('color')
+    second.style.color = 'black'
+}
